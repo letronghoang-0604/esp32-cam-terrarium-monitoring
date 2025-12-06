83bbfc2ffa65bafc1e52219253a324d0f5ac2ef6
Lê Trọng Hoàng <letronghoang0604@gmail.com>
2025-12-06 11:27:02 +0700

    Delete code directory

--- code/index_html.h
+++
@@ -1,8 +0,0 @@
-## Getting the Web UI Source (index_html.h)
-
-For the full web dashboard source code (`index_html.h`), please contact me directly:
-
-- Email: letronghoang0604@gmail.com  
-- Zalo: 0903240604  
-
-I will be happy to share the `index_html.h` file and instructions on how to integrate it into the project.
